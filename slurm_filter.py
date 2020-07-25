e37f8bef5e1d418f3988dbe6d82b6f86ee59483c
Mihaly Kollo <kollom@vis000.camp.thecrick.org>
2020-07-25 16:42:27 +0100

    Slurm filtering added

--- slurm_filter.py
+++ slurm_filter.py
@@ -23,9 +23,10 @@ import cupy
 import cusignal
 
 MASTER_PROCESS = 0
-SETUP_TAG = 1
-WORK_TAG = 2
-DIE_TAG = 3
+SHAPE_TAG = 71
+AMPS_TAG = 72
+DATA_TAG = 73
+DIE_TAG = 74
 
 sample_batch_size=65536
 cmr=False
@@ -35,13 +36,14 @@ high_cutoff=9000
 amp_list=np.ones((1024))*250
 channel_scales=1000/amp_list
 
-sos = butter(order, [low_cutoff/10000, high_cutoff/10000], 'bandpass', output='sos')
 
 comm = MPI.COMM_WORLD
 nproc = comm.Get_size()
 iproc = comm.Get_rank()
 inode = MPI.Get_processor_name()
 status = MPI.Status()
+
+sos = butter(order, [low_cutoff/10000, high_cutoff/10000], 'bandpass', output='sos')
 
 comm.Barrier()
 if iproc == MASTER_PROCESS:
@@ -56,58 +58,52 @@ if iproc == MASTER_PROCESS:
     sample_chunk_borders=np.hstack((np.arange(n_sample_chunks, dtype=int)*sample_batch_size,n_samples))
     sample_chunks=dict(zip(np.arange(nproc-1),[np.array([sample_chunk_borders[i:i+2].copy() for i in range(len(sample_chunk_borders)-1)])]*(nproc-1)))
     current_chunks=dict(zip(np.arange(11),[None]*(nproc-1)))
-    n_all_chunks=sum([sample_chunks[i].shape[0] for i in np.arange(nproc-1)])
-    n_total_chunks=sum([sample_chunks[i].shape[0] for i in np.arange(3)])
-    for i, channel_per_proc in enumerate([len(c) for c in channel_chunks]):
-        comm.send(channel_per_proc, dest=i+1, tag=SETUP_TAG)
-    while (sum([sample_chunks[i].shape[0] for i in np.arange(nproc-1)])+sum([cc is not None for cc in current_chunks]))>0:
-        print((1-sum([sample_chunks[i].shape[0] for i in np.arange(3)])/n_total_chunks)*1000, flush=True)
-        print(current_chunks, flush=True)
+    n_total_chunks=sum([sample_chunks[i].shape[0] for i in np.arange(nproc-1)])
+    n_remaining_chunks=n_total_chunks
+    n_current_chunks=0
+    while (n_remaining_chunks+n_current_chunks)>0:
         for i, _ in enumerate(current_chunks):
-            ch=current_chunks[i]
-            print("current chunk for "+str(i)+" is "+str(ch), flush=True)
-            print("all chunks for "+str(i)+" are "+ str(sample_chunks[i].shape[0]), flush=True)
-            print("Sample chunks reamining size: "+str(sample_chunks[i]), flush=True)
-            if ch==None and sample_chunks[i].shape[0]>0:
-                batch=np.zeros((channel_chunks[i].shape[0],sample_batch_size*2+1))
+            if current_chunks[i] is None and sample_chunks[i].shape[0]>0:
                 current_chunks[i]=sample_chunks[i][0]
-                sample_chunks[i]=np.delete(sample_chunks[i], (0), axis=0)
-                print("Master made chunks", flush=True)
+                data_shape=(channel_chunks[i].shape[0], sample_batch_size+current_chunks[i][1]-current_chunks[i][0])
+                comm.send(data_shape, dest=i+1, tag=SHAPE_TAG)
+                comm.send(channel_scales[channel_chunks[i]], dest=i+1, tag=AMPS_TAG)
+                batch=np.empty((channel_chunks[i].shape[0],sample_batch_size+current_chunks[i][1]-current_chunks[i][0]))
                 if current_chunks[i][0]==0:
                     batch[:,:sample_batch_size]=np.array([in_file['sig'][channel_chunks[i],0],]*sample_batch_size).transpose()
                 else:
-                    batch[:,:sample_batch_size]=in_file['sig'][channel_chunks[i],]
-                batch[:,sample_batch_size:-1]=in_file['sig'][channel_chunks[i],current_chunks[i][0]:current_chunks[i][1]]
-                batch[:,-1]=channel_scales[channel_chunks[i]]
-                print("Master loaded batch from file")
-                print("Master sending data to proc "+str(i+1) + " of size "+str(batch.shape), flush=True)
-                comm.send(batch, dest=i+1, tag=WORK_TAG)
-                print("Master sent data to proc "+str(i+1), flush=True)
+                    batch[:,:sample_batch_size]=in_file['sig'][channel_chunks[i],(current_chunks[i][0]-sample_batch_size):current_chunks[i][0]]
+                sample_chunks[i]=np.delete(sample_chunks[i], (0), axis=0)
+                batch[:,sample_batch_size:]=in_file['sig'][channel_chunks[i],current_chunks[i][0]:current_chunks[i][1]]
+                comm.send(batch, dest=i+1, tag=DATA_TAG)
+                
         data=comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
         rnk = status.Get_source()
-        print("Dset shape "+str(out_dset[channel_chunks[i],current_chunks[rnk-1][0]:current_chunks[rnk-1][1]].shape),flush=True)
-        print("sample_batch_size "+str(sample_batch_size),flush=True)
-        print("Data shape 3 "+str(data.shape),flush=True)
+        print("Data from "+str(rnk)+" shape: "+str(data.shape))
+        print("Data from "+str(rnk)+" out  : "+str(out_dset[channel_chunks[rnk-1],current_chunks[rnk-1][0]:current_chunks[rnk-1][1]].shape))
+
         out_dset[channel_chunks[rnk-1],current_chunks[rnk-1][0]:current_chunks[rnk-1][1]]=data
         current_chunks[rnk-1]=None
+        print(sum([sample_chunks[i].shape[0] for i in np.arange(nproc-1)])+sum([cc is not None for cc in current_chunks]))
+        print(sum([sample_chunks[i].shape[0] for i in np.arange(nproc-1)]))
+        print(sum([cc is not None for cc in current_chunks]))
+        print(current_chunks)
+        n_current_chunks=sum([current_chunks[cc] is not None for cc in current_chunks])
+        n_remaining_chunks=sum([sample_chunks[i].shape[0] for i in np.arange(nproc-1)])
+        
     for proc in range(nproc-1):
         comm.send(-1, proc, tag=DIE_TAG)    
 else: 
-    print("Process "+str(iproc)+" waiting for setup", flush=True)
-    proc_n_channels=comm.recv(source=0, tag=SETUP_TAG)
-    print("Process "+str(iproc)+" received setup: "+str(proc_n_channels))
-    batch = np.empty((proc_n_channels,sample_batch_size*2+1), dtype='int16')
     continue_working = True
     while continue_working:
-        print("Process "+str(iproc)+" waiting for data of size "+str(batch.shape), flush=True)
-        batch=comm.recv(source=0, tag=WORK_TAG, status=status)    
+        batch_shape=comm.recv(source=MASTER_PROCESS, tag=SHAPE_TAG)    
+        amps=comm.recv(source=MASTER_PROCESS, tag=AMPS_TAG)            
+        batch=comm.recv(source=0, tag=DATA_TAG, status=status)    
         tag = status.Get_tag()
-        print("Process "+str(iproc)+" received data", flush=True)
         if tag == DIE_TAG:
             continue_working = False
         else:
-            print("Proc "+str(iproc)+" received data", flush=True)
-            cusig=cupy.asarray(batch[:,:-1], dtype=cupy.float32)
+            cusig=cupy.asarray(batch, dtype=cupy.float32)
             cusig=cusig-cupy.mean(cusig)
             if cmr:
                 cusig=cusig-cuda_median(cusig,0) 
@@ -117,16 +113,8 @@ else:
             cusig=cupy.flipud(cusig)
             proc_channel_scales=cupy.asarray(batch[:,-1], dtype=cupy.float32)[:,None]
             cusig=cusig*proc_channel_scales
-            result_array=cupy.asnumpy(cusig[:,int((batch.shape[1]-1)/2):])
-            print("Proc "+str(iproc)+" calculated result", flush=True)
-            print("Batch shape " + str((batch.shape[1]-1)/2),flush=True)
-
-            print("Result array size "+str(result_array.shape),flush=True)
+            result_array=cupy.asnumpy(cusig[:,-(batch.shape[1]-sample_batch_size):])
             comm.send(result_array, dest=MASTER_PROCESS, tag=iproc)
-            print("Proc "+str(iproc)+" sent result", flush=True)
-
-            
-comm.Barrier()
 
 if iproc == MASTER_PROCESS:
     print(int(1000), flush=True)
