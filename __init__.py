949becca2f5c772260e711a6986a6ebe7f61e082
Tom Warner <tom.p.a.warner@gmail.com>
2020-12-11 16:30:24 +0000

    ditto

--- __init__.py
+++ __init__.py
@@ -4,6 +4,4 @@ from .recording import StimRecording
 from .plotting import plot_chip_surface_amps
 from .plotting import plot_chip_surface_clusters
 from .experiment import Experiment
-from .ramdisk import create_clone
-from .ramdisk import wipe
 from .slurm import Slurm
