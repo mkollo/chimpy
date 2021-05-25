8aa6fc5ad7a1d9d738fd168458e7f76465e6c732
Tom Warner <warnert@login000.camp.thecrick.org>
2021-05-25 19:18:31 +0100

    dask filter

--- dask_tasks.py
+++ dask_tasks.py
@@ -1,12 +1,17 @@
 import numpy as np
 from tqdm import trange
 import dask.array as da
-import cupy as cp
+import cupy
 import cusignal
+from scipy.signal  import butter
+import h5py
+import time
+
 
 def dask_tcs(recording, chunk_size=65536, overlap=1000, order=40, threshold=4, return_stds=False,  whitening=False):
     all_spikes = [[], []]
     spike_amps = []
+    
     stds = np.std(recording.filtered_data(from_sample=0, to_sample=chunk_size), axis=1)
     for i in trange(round(recording.sample_length/chunk_size)):
 
@@ -14,17 +19,88 @@ def dask_tcs(recording, chunk_size=65536, overlap=1000, order=40, threshold=4, r
         chunk = da.array(chunk)
         if whitening:
             U, S, Vt = da.linalg.svd(chunk)
-            chunk = cp.dot(U.compute(), Vt.compute())
+            chunk = cupy.dot(U.compute(), Vt.compute())
         chunk_spikes = cusignal.peak_finding.peak_finding.argrelmin(chunk, order=order, axis=1)
         spike_vals = chunk[chunk_spikes[0].get(), chunk_spikes[1].get()]
         sig_spikes = np.where(spike_vals <= - threshold*stds[chunk_spikes[0].get()])[0]
         all_spikes[0].append(chunk_spikes[0][sig_spikes])
         all_spikes[1].append(chunk_spikes[1][sig_spikes]+i*chunk_size)
         spike_amps.append(spike_vals[sig_spikes])
-    all_spikes = cp.array([cp.concatenate(all_spikes[0]), cp.concatenate(all_spikes[1])])
+    all_spikes = cupy.array([cupy.concatenate(all_spikes[0]), cupy.concatenate(all_spikes[1])])
     if return_stds:
         return all_spikes, spike_amps, stds
     else:
         return all_spikes, spike_amps
 
+
+
+
+def dask_filter_chunk(in_rec_filepath, channels, idx_from, idx_to, scales, low_cutoff, high_cutoff, order=3, cmr=True, whiten=True, h5write=None):
+    sos = butter(order, [low_cutoff/10000, high_cutoff/10000], 'bandpass', output='sos')
+    file = h5py.File(in_rec_filepath, 'r')
+    sig = file['sig']
+    chunk_size = idx_to - idx_from
+    if idx_to > sig.shape[1]:
+        idx_to = sig.shape[1]
+        idx_from = idx_to - chunk_size
+    if idx_from == 0:
+        chunk = np.ones((len(channels), chunk_size*2))*sig[channels, 0][:, np.newaxis]
+        chunk[:, chunk_size:] = sig[channels, :idx_to]
+    else:
+        chunk = sig[channels, idx_from-chunk_size:idx_to]
+    saturations = np.count_nonzero(((0==chunk[:, chunk_size:]) | (chunk[:, chunk_size:] == 4095)), axis=1)
+    file.close()
+    cusig = cupy.asarray(chunk, dtype=cupy.float32)
+    cusig = cusig - cupy.mean(cusig)
+    cusig=cusignal.sosfilt(sos,cusig)
+    cusig=cupy.fliplr(cusig)
+    cusig=cusignal.sosfilt(sos,cusig)
+    cusig=cupy.fliplr(cusig)
+    cusig=cusig*cupy.asarray(scales, dtype=cupy.float32)[:,None]
+    if cmr:
+        cusig=cusig-cupy.median(cusig,axis=0)
+    cusig = cusig.get()
+    if whiten:
+        U, S, Vt = np.linalg.svd(cusig, full_matrices=False)
+        w_chunk = np.dot(U, Vt)
+        if h5write is not None:
+            written=False
+            while not written:
+                try:
+                    out_fid = h5py.File(h5write, 'r+')
+                    out_fid['sig'][:, idx_from:idx_to] = cusig[:, chunk_size:]
+                    out_fid['white_sig'][:, idx_from:idx_to] = w_chunk[:, chunk_size:]
+                    out_fid.close()
+                    written = True
+                except:
+                    time.sleep(0.05)
+            return saturations
+        else:
+            return cusig, w_chunk, saturations
+    else:
+        if h5write is not None:
+            written=False
+            while not written:
+                try:
+                    out_fid = h5py.File(h5write, 'r+')
+                    if idx_from == 0:
+                        out_fid['sig'][:, :idx_to] = cusig[:, :idx_to]
+                    out_fid['sig'][:, int(idx_from+chunk_size/2):idx_to] = cusig[:, int(chunk_size/2):]
+                    out_fid.close()
+                    written = True
+                except:
+                    time.sleep(0.05)
+            return saturations
+        else:
+            return cusig, saturations
     
+
+def check_progress(dask_outs, progress_plot=False):
+    status = [i.status for i in dask_outs]
+    while len(np.where(np.array(status) == 'finished')[0]) != len(status):
+        status = [i.status for i in dask_outs]
+        print('Completion:', len(np.where(np.array(status) == 'finished')[0])/len(status), end='\r')
+        time.sleep(0.05)
+        if any(np.array(status) == 'error') or any(np.array(status) == 'lost'):
+            print('error on task')
+            break
--- preprocess.py
+++ preprocess.py
@@ -20,6 +20,8 @@ import importlib
 import h5py
 from chimpy.slurm import Slurm
 from chimpy.ramfile import RamFile
+from chimpy.dask_tasks import dask_filter_chunk, check_progress
+import dask
 
 def cuda_median(a, axis=1):
     a = cupy.asanyarray(a)
@@ -70,6 +72,83 @@ def filter_traces(s, low_cutoff, high_cutoff, order=3, cmr=False, sample_chunk_s
         output[:,idx_from:idx_to]=cupy.asnumpy(cusig[:,overlap:])
         chunk[:,:overlap]=chunk[:,-overlap:]
     return output
+
+
+
+def filter_dask(in_recording, stim_recording, low_cutoff, high_cutoff, client, order=3, sample_chunk_size=65536, n_samples=-1, cmr=True, whiten=True, dask_write=False, show_progress=False):
+    
+    # Set up the parameters from the recordings
+    channels=stim_recording.channels
+    amps=stim_recording.amps
+    scales=1000/amps
+    n_channels=stim_recording.channels.shape[0]
+    # Optionally save file into a tmpfs partition for processing
+    in_filepath=in_recording.filepath
+    out_filepath=in_recording.filtered_filepath
+
+    # Open the input
+    in_fid=h5py.File(in_filepath, 'r')
+    # Create output file
+    out_fid=h5py.File(out_filepath, 'w')
+    if n_samples==-1:
+        n_samples=in_fid['sig'].shape[1]
+    out_mapping=in_fid['mapping'][stim_recording.connected_in_mapping]
+    for i, m in enumerate(out_mapping):
+        m[0]=i    
+    out_fid['mapping']=out_mapping
+
+    # Copy over the parameters over
+    in_fid.copy('/message_0', out_fid)
+    in_fid.copy('/proc0', out_fid)
+    in_fid.copy('/settings', out_fid)
+    in_fid.copy('/time', out_fid)
+    in_fid.copy('/version', out_fid)
+    if 'bits' in in_fid.keys():
+        in_fid.copy('/bits', out_fid)
+
+    # New dataset for the filtered data and whitened if whitening is True
+    out_fid.create_dataset("sig", (n_channels, n_samples), dtype='float32')
+    if whiten:
+        out_fid.create_dataset('white_sig', (n_channels, n_samples), dtype='float32')
+    # Create chunks
+
+    # Create the number of chunks
+    n_sample_chunks=n_samples/sample_chunk_size
+    sample_chunks=np.hstack((np.arange(n_sample_chunks, dtype=int)*sample_chunk_size,n_samples))
+
+    # Dataset for saturations and the first frame
+    out_fid.create_dataset('saturations', (n_channels, len(sample_chunks-1)), dtype='int32')
+    out_fid.create_dataset('first_frame', shape=(1,), data=in_fid["sig"][1027,0]<<16 | in_fid["sig"][1026,0])
+
+    # Create a series of dask delayed to do the filtering
+    h5write = None
+    if dask_write:
+        h5write = out_filepath
+    filters = [dask.delayed(dask_filter_chunk)(in_recording.filepath, channels, sample_chunks[i], sample_chunks[i+1], scales, low_cutoff, high_cutoff,order=order, cmr=cmr, whiten=whiten, h5write=h5write) for i in range(len(sample_chunks)-1)]
+
+    # Close the out fid if the dask delayed will write to the output
+    if dask_write:
+        out_fid.close()
+
+    # Compute and show progress
+    filters_out = client.compute(filters)
+    if show_progress:
+        check_progress(filters_out)
+    
+    # Write to the out_fid if the dask tasks aren't doing it
+    out_fid=h5py.File(out_filepath, 'a')
+    if not dask_write:
+        if whiten:
+            for i in trange(len(sample_chunks)-1):
+                out_fid['white_sig'][:, sample_chunks[i]:sample_chunks[i+1]] = filters_out[i].result()[1][:, sample_chunk_size:]
+        for i in trange(len(sample_chunks)-1):
+            out_fid['sig'][:, sample_chunks[i]:sample_chunks[i+1]] = filters_out[i].result()[0][:, sample_chunk_size:]
+            out_fid['saturations'][:, i] = filters_out[i].result()[-1]
+    else:
+        for i in trange(len(sample_chunks)-1):
+            out_fid['saturations'][:, i] = filters_out[i].result()
+    out_fid.close()
+    return filters_out
 
 def filter_experiment_slurm(in_recording, stim_recording, low_cutoff, high_cutoff, order=3, sample_chunk_size=65536, n_samples=-1, ram_copy=False):
     #     Optionally save file into a tmpfs partition for processing   
@@ -142,14 +221,14 @@ def filter_experiment_local(in_recording, stim_recording, low_cutoff, high_cutof
         chunk[:,overlap:]=in_fid['sig'][channels,idx_from:idx_to]
         out_fid['saturations'][:, i] = np.count_nonzero(((0==chunk[:, overlap:]) | (chunk[:, overlap:] == 4095)), axis=1)
         cusig=cupy.asarray(chunk, dtype=cupy.float32)
-        cusig=cusig-cupy.mean(cusig)
-        if cmr:
-            cusig=cusig-cuda_median(cusig,0) 
+        cusig=cusig-cupy.mean(cusig) 
         cusig=cusignal.sosfilt(sos,cusig)
         cusig=cupy.fliplr(cusig)
         cusig=cusignal.sosfilt(sos,cusig)
         cusig=cupy.fliplr(cusig)
         cusig=cusig*cupy.asarray(scales, dtype=cupy.float32)[:,None]
+        if cmr:
+            cusig=cusig-cupy.median(cusig,axis=0)
         out_fid["sig"][:,idx_from:idx_to]=cupy.asnumpy(cusig[:,overlap:])
         chunk[:,:overlap]=chunk[:,-overlap:]
 #     Writing filtered traces to disk...
@@ -186,3 +265,4 @@ def power_spectrum(data):
     power_spectrum_smoothed=np.convolve(power_spectrum, np.ones((20,))/20, mode='valid')
     frequency = np.linspace(0, 10000, len(power_spectrum_smoothed))
     return frequency, power_spectrum_smoothed
+
