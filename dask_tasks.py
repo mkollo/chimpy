7aa29a9a5405c0c8d8d33e415e98df1a051a6449
Tom Warner <tom.p.a.warner@gmail.com>
2021-09-02 16:59:04 +0100

    update dask tasks

--- dask_tasks.py
+++ dask_tasks.py
@@ -1,55 +1,67 @@
 import numpy as np
 from tqdm import trange
 import dask.array as da
-import cupy
-import cusignal
+try:
+    import cupy
+    import cusignal
+except:
+    pass
 from scipy.signal  import butter
 import h5py
 import time
 
 
-def dask_tcs(recording, chunk_size=65536, overlap=1000, order=40, threshold=4, return_stds=False,  whitening=False):
-    all_spikes = [[], []]
-    spike_amps = []
-    
-    stds = np.std(recording.filtered_data(from_sample=0, to_sample=chunk_size), axis=1)
-    for i in trange(round(recording.sample_length/chunk_size)):
-
-        chunk = recording.filtered_data(from_sample=i*chunk_size, to_sample=(i+1)*chunk_size+overlap)
-        chunk = da.array(chunk)
-        if whitening:
-            U, S, Vt = da.linalg.svd(chunk)
-            chunk = cupy.dot(U.compute(), Vt.compute())
-        chunk_spikes = cusignal.peak_finding.peak_finding.argrelmin(chunk, order=order, axis=1)
-        spike_vals = chunk[chunk_spikes[0].get(), chunk_spikes[1].get()]
-        sig_spikes = np.where(spike_vals <= - threshold*stds[chunk_spikes[0].get()])[0]
-        all_spikes[0].append(chunk_spikes[0][sig_spikes])
-        all_spikes[1].append(chunk_spikes[1][sig_spikes]+i*chunk_size)
-        spike_amps.append(spike_vals[sig_spikes])
-    all_spikes = cupy.array([cupy.concatenate(all_spikes[0]), cupy.concatenate(all_spikes[1])])
-    if return_stds:
-        return all_spikes, spike_amps, stds
+def dask_tcs(recording_filepath, idx_from, idx_to, chans, stds,  order=40, threshold=4,whitening=False):
+    file = h5py.File(recording_filepath, 'r')
+    if whitening:
+        data = file['white_sig']
     else:
-        return all_spikes, spike_amps
+        data = file['sig']
+#    chunk_size = idx_to - idx_to
+    chunk = data[chans, idx_from:idx_to]
+    file.close()
+    chunk_spikes = cusignal.peak_finding.peak_finding.argrelmin(chunk, order=order, axis=1)
+    spike_vals = chunk[chunk_spikes[0].get(), chunk_spikes[1].get()]
+    sig_spikes = np.where(spike_vals <= - threshold*stds[chunk_spikes[0].get()])[0]
+    return chunk_spikes[0][sig_spikes], chunk_spikes[1][sig_spikes]+idx_from, spike_vals[sig_spikes]
 
-
-
+def dask_wf(recording_filepath, spike_time, channels, window=30, whitened=False):
+    file = h5py.File(recording_filepath, 'r')
+    data = file['sig'][channels, int(spike_time-window):int(spike_time+window)]
+    if whitened:
+        w_data = file['white_sig'][channels, int(spike_time-window):int(spike_time+window)]
+    file.close()
+    if whitened:
+        return data, w_data
+    else:
+        return data
 
 def dask_filter_chunk(in_rec_filepath, channels, idx_from, idx_to, scales, low_cutoff, high_cutoff, order=3, cmr=True, whiten=True, h5write=None):
+    '''
+    Filters a chunk of data from a h5py recording, designed to be used with dask
+    '''
+
+    # Generate filter
     sos = butter(order, [low_cutoff/10000, high_cutoff/10000], 'bandpass', output='sos')
+
+    # Open the file and extract the sig
     file = h5py.File(in_rec_filepath, 'r')
     sig = file['sig']
+
+    # Size of the chunk we're going to use. Take a chunk twice as big as this to allow for smooth filtering.
     chunk_size = idx_to - idx_from
     if idx_to > sig.shape[1]:
         idx_to = sig.shape[1]
         idx_from = idx_to - chunk_size
-    if idx_from == 0:
+    if idx_from == 0: # Make a chunk of dc to prevent weird edge filtering problems
         chunk = np.ones((len(channels), chunk_size*2))*sig[channels, 0][:, np.newaxis]
         chunk[:, chunk_size:] = sig[channels, :idx_to]
     else:
         chunk = sig[channels, idx_from-chunk_size:idx_to]
+    # Count saturations in the chunk
     saturations = np.count_nonzero(((0==chunk[:, chunk_size:]) | (chunk[:, chunk_size:] == 4095)), axis=1)
     file.close()
+    # Filtering with CUDA
     cusig = cupy.asarray(chunk, dtype=cupy.float32)
     cusig = cusig - cupy.mean(cusig)
     cusig=cusignal.sosfilt(sos,cusig)
@@ -59,7 +71,7 @@ def dask_filter_chunk(in_rec_filepath, channels, idx_from, idx_to, scales, low_c
     cusig=cusig*cupy.asarray(scales, dtype=cupy.float32)[:,None]
     if cmr:
         cusig=cusig-cupy.median(cusig,axis=0)
-    cusig = cusig.get()
+    cusig = cusig.get() # Conver to numpy
     if whiten:
         U, S, Vt = np.linalg.svd(cusig, full_matrices=False)
         w_chunk = np.dot(U, Vt)
@@ -99,8 +111,8 @@ def check_progress(dask_outs, progress_plot=False):
     status = [i.status for i in dask_outs]
     while len(np.where(np.array(status) == 'finished')[0]) != len(status):
         status = [i.status for i in dask_outs]
-        print('Completion:', len(np.where(np.array(status) == 'finished')[0])/len(status), end='\r')
-        time.sleep(0.05)
+        print('Dask Completion:', len(np.where(np.array(status) == 'finished')[0])/len(status), end='\r')
+        time.sleep(0.5)
         if any(np.array(status) == 'error') or any(np.array(status) == 'lost'):
             print('error on task')
             break
